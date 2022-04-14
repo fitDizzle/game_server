5af6b5589c6d521bcba959934a8a96b1c28fefd0
fitDizzle <markfitnessaz@gmail.com>
2022-04-13 18:08:39 -0700

    added to config URI

--- database/models/index.js
+++ database/models/index.js
@@ -6,13 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://rgsfmkdctjvykl:af3b2530cd791b741a38e290e023818e2a9990a0f4ccdd88bf01ae3d06900505@ec2-34-207-12-160.compute-1.amazonaws.com:5432/d4h5o5seoueaft',
-  {
-    logging: false,
-    ssl: { rejectUnauthorized: false } //solved the problem with self signed sertificate
-}    
-);
+const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
