006b0e484fc6494ece3ca1a69ca56a0a9e7b33a6
Mark Clark <mark.clark@gusto.com>
2023-11-04 18:41:13 -0700

    feat: update db index

--- database/models/index.js
+++ database/models/index.js
@@ -1,44 +1,26 @@
-'use strict';
 require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = new Sequelize(
-  process.env.DATABASE_URL, {
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
   host: process.env.HOST,
   dialect: 'postgres',
   dialectOptions: {
-    ssl: { rejectUnauthorized: false }
-  }
-}
-);
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
-db.sequelize = sequelize;
+const authenticate_database_connection = async () => {
+  try {
+    await db.authenticate();
+    console.log('Database connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    throw error;
+  }
+};
 
-module.exports = db;
+authenticate_database_connection();
+
+module.exports = { db, authenticate_database_connection };
